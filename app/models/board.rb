f2441c3b0d8b14157c40e540101b72694431b381
Kevin <kevin.calva@us.isobar.com>
2024-06-21 14:24:00 -0500

    adding validations and some editing on html to get it accurate

--- app/models/board.rb
+++ app/models/board.rb
@@ -8,4 +8,15 @@
 #  updated_at :datetime         not null
 #
 class Board < ApplicationRecord
+  validates(:name,{:presence=>true,:uniqueness=>true})
+  has_many(:posts)
+
+  def active_posts_in_ascending_order
+    posts.where("expires_on >= ?",Date.today).order(expires_on: :asc)
+  end
+
+  def expired_posts_in_ascending_order
+    # posts.where("expires_on < ?",Date.today).order(:expires_on).reverse
+    posts.where("expires_on < ?",Date.today).order(expires_on: :desc)
+  end
 end
